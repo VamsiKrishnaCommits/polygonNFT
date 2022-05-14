77a3b80a0da7e57d6765b142942b17c3b2bb5d1a
vamsi <vamsi.scientist@gmail.com>
2022-05-15 00:52:53 +0530

    Second Commit

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const HomeMadeNFT = await hre.ethers.getContractFactory("HomeMadeNFT");
+  const homeMadeNFT = await HomeMadeNFT.deploy();
 
-  await greeter.deployed();
+  await homeMadeNFT.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("HomeMadeNFT deployed to:", homeMadeNFT.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
